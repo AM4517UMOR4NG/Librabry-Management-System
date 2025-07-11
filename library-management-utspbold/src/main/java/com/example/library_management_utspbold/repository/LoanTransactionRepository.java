fe60df4a03b833d0a3c7989c24fd1f9d78ab04c7
AM4517UMOR4NG <aekmohop@gmail.com>
2025-07-11 19:34:39 +0700

    Push Class member and Transaction

--- library-management-utspbold/src/main/java/com/example/library_management_utspbold/repository/LoanTransactionRepository.java
+++ library-management-utspbold/src/main/java/com/example/library_management_utspbold/repository/LoanTransactionRepository.java
@@ -1,9 +1,14 @@
 package com.example.library_management_utspbold.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.library_management_utspbold.model.LoanTransaction;
 
 public interface LoanTransactionRepository extends JpaRepository<LoanTransaction, Long> {
-
+    @Query("SELECT l FROM LoanTransaction l JOIN l.member m JOIN m.user u WHERE u.username = :username ORDER BY l.loanDate DESC")
+    Page<LoanTransaction> findByMemberUserUsername(@Param("username") String username, Pageable pageable);
 }
--- library-management-utspbold/src/main/java/com/example/library_management_utspbold/repository/MemberRepository.java
+++ library-management-utspbold/src/main/java/com/example/library_management_utspbold/repository/MemberRepository.java
@@ -3,6 +3,8 @@ package com.example.library_management_utspbold.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.library_management_utspbold.model.Member;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUser_Username(String username);
 }
